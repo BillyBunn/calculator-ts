d50107fb5134fbca515c273a4f266b54f77182a6
Billy Bunn <40321457+BillyBunn@users.noreply.github.com>
2019-09-09 13:52:41 -0700

    adds footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import Display from "./Display";
 import Buttons from "./Buttons";
-
 import Calculator from "./styles/Calculator";
+
+const Footer = styled.footer`
+  display: flex;
+  flex-flow: column nowrap;
+  margin-top: 2rem;
+  text-align: center;
+  > * {
+    margin-bottom: 0.5rem;
+  }
+`;
 
 const App = () => {
   return (
-    <Calculator>
-      <header>TypeScript Calculator</header>
-      <Display />
-      <Buttons />
-    </Calculator>
+    <>
+      <Calculator>
+        <header>TypeScript Calculator</header>
+        <Display />
+        <Buttons />
+      </Calculator>
+      <Footer>
+        <span>
+          Built with{" "}
+          <span role="img" aria-label="Love">
+            ❤️
+          </span>{" "}
+          by <a href="https://billybunn.com/">Billy Bunn</a>.
+        </span>
+        <span>
+          <a href="https://github.com/BillyBunn/calculator-ts">
+            See it on GitHub
+          </a>
+        </span>
+      </Footer>
+    </>
   );
 };
 
