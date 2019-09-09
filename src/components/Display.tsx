d3ca0fb4d8b3c471ecaa707ae002239f741e91ca
Billy Bunn <40321457+BillyBunn@users.noreply.github.com>
2019-09-08 21:03:54 -0700

    updates display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -21,16 +21,24 @@ const StyledDisplay = styled.div`
 `;
 const Display = () => {
   const [state] = React.useContext<[State, React.Dispatch<Action>]>(Context);
+  const [showMore, setShowMore] = React.useState(false);
   return (
     <StyledDisplay className="display">
-      {/* <div>{state.display}</div> */}
-      <ul>
-        <li>display: {`${state.display}`}</li>
-        <li>operator: {`${state.operator}`}</li>
-        <li>firstOperand: {`${state.firstOperand}`}</li>
-        <li>waitingForSecondOperand: {`${state.waitingForSecondOperand}`}</li>
-        <li>memory: {`${state.memory}`}</li>
-      </ul>
+      <div onClick={() => setShowMore(!showMore)}>
+        {showMore ? (
+          <ul>
+            <li>display: {`${state.display}`}</li>
+            <li>operator: {`${state.operator}`}</li>
+            <li>firstOperand: {`${state.firstOperand}`}</li>
+            <li>
+              waitingForSecondOperand: {`${state.waitingForSecondOperand}`}
+            </li>
+            <li>memory: {`${state.memory}`}</li>
+          </ul>
+        ) : (
+          state.display
+        )}
+      </div>
     </StyledDisplay>
   );
 };
